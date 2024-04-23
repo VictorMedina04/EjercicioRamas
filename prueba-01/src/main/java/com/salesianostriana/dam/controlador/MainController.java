3faab8043905c42f45170a4e6d5ea1fffaa256da
Victor <medina.levic24@triana.salesianos.edu>
2024-04-23 09:13:03 +0200

    mapping

--- prueba-01/src/main/java/com/salesianostriana/dam/controlador/MainController.java
+++ prueba-01/src/main/java/com/salesianostriana/dam/controlador/MainController.java
@@ -2,15 +2,18 @@ package com.salesianostriana.dam.controlador;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+	@GetMapping("/")
 	public String holaMundo(Model model) {
 		model.addAttribute("mensaje", "Hola mundo");
 		return "inicio";
 	}
 
+	@GetMapping("/")
 	public String alagos(Model model) {
 		model.addAttribute("mensaje2", "Angel y Luismi los mejores profesores de todo salesianos ");
 		return "inicio";
